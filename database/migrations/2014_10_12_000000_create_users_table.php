9b1e03c3ad3723e054a98e846e174f897be078f9
acroziqin <achmadchoirurroziqin@gmail.com>
2019-01-25 19:08:26 +0700

    password nullable

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -18,7 +18,7 @@ class CreateUsersTable extends Migration
             $table->string('name');
             $table->string('email')->unique();
             $table->timestamp('email_verified_at')->nullable();
-            $table->string('password');
+            $table->string('password')->nullable();
             $table->string('alamat')->nullable();
             $table->string('no_hp')->nullable();
             $table->string('jenis_kelamin')->nullable();
